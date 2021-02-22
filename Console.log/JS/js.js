ab93ecc1133c300d78f6077e28787b479d2c1bd2
CarlaMSLopes <carlalopes613@outlook.com>
2021-02-22 20:47:32 -0300

    Console.log e comentários
    Aula 01

---
+++ Console.log/JS/js.js
@@ -0,0 +1,15 @@
+console.log('Carla "Lopes"'); //string = texto 
+console.log("Carla 'Lopes'");
+console.log(`'Carla' "Lopes"`);
+
+console.log(35, 15.85);  //number =número
+console.log('Meu nome é "Carla Lopes". Estou aprendendo JavaScript ás', 20, 'da manhã');
+
+// Duas barras acrescenta comentários. Tudo que estiver depois dessas duas barras não aparece no console, ele é ignorado pelo code. 
+/*Comentários mais longos em blocos*/
+// Posso cancelar linhas de código se comentar elas.
+//console.log('Esse código não vai aparecer');
+console.log("Arroz"); //pedra
+console.log("Feijão"); // mar
+
+
