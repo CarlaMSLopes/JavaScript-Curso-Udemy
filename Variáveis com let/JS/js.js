571a7367c9fcc4a7bf854b7299721271547c2480
CarlaMSLopes <carlalopes613@outlook.com>
2021-02-23 17:13:06 -0300

    Variáveis com let e const
    let e const

---
+++ Variáveis com let/JS/js.js
@@ -0,0 +1,25 @@
+//let
+let nome = 'João';  //variável do tipo string
+
+console.log(nome, 'nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+//nome = 'Carla'; let pode ser alterado;
+
+
+//const
+
+const nome1 = 'Pablo';
+console.log(nome1);
+
+const primeiroNumero = 5; //variável do tipo nomber
+const segundoNumero = 10;
+const conta = primeiroNumero * segundoNumero;
+const contaDuplicada = conta * 2;
+console.log(contaDuplicada);
+
+//ou desse jeito
+
+console.log(primeiroNumero + segundoNumero);
